7f5d55f0031e5ed1f74585bb037b03c37f48a5df
gekkedev <17025257+gekkedev@users.noreply.github.com>
2025-08-24 19:52:56 +0200

    fix: cmd scripts exiting after error

--- docker/run.bat
+++ docker/run.bat
@@ -1,3 +1,2 @@
 REM for powershell: docker run --rm -v ${PWD}/../video:/app/video deoldify python VideoColorizer.py
-docker run --rm -v %cd%\..\video:/app/video deoldify python VideoColorizer.py
-PAUSE
+docker run --rm -v %cd%\..\video:/app/video deoldify python VideoColorizer.py & PAUSE
---
+++ run.bat
@@ -0,0 +1,1 @@
+python VideoColorizer.py & PAUSE
